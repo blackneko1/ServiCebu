6f718521a2dff0de8b9c63a9697fd22b1a095477
blackneko <blackneko@DESKTOP-75DH29A>
2021-03-21 21:09:30 +0800

    testtest

---
+++ app/config/config.php
@@ -0,0 +1,15 @@
+<?php 
+    //Database params
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASS', '');
+    define('DB_NAME', 'servicebu');
+    //APPROOT
+    define('APPROOT', dirname(dirname(__FILE__)));
+
+    //URLROOT
+    define('URLROOT', 'localhost/mvcframework');
+
+    //Sitename
+    define('SITENAME', 'ServiCebu');
+?>
--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -1,8 +1,18 @@
 <?php 
-    class Pages {
+    class Pages extends Controller {
         public function __construct(){
-            
+            $this->userModel = $this->model('User');
         }
 
+        public function index(){
+            $data = [
+                'title' => 'Home page',
+                'name' => 'Dary'
+            ];
+            $this->view('pages/index', $data);
+        }
+        public function about(){
+            $this->view('pages/about');
+        }
     }
 ?>
